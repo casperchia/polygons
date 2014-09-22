8a4a0af4a11b6781f1a808a4056eee0c3d444805
Matt <mciancio@live.com.au>
2014-09-22 14:38:03 +1000

    Created the Acad_Obj_Group model, which is based off the UNSW acad_obj_group table. The Acad_Obj_Group_Type model is based off the UNSW custom domain "acadobjectgrouptype".

--- src/polygons/models/__init__.py
+++ src/polygons/models/__init__.py
@@ -2,3 +2,5 @@ from Org_Unit_Type import Org_Unit_Type
 from Org_Unit import Org_Unit
 from Career import Career
 from Program import Program
+from Acad_Obj_Group_Type import Acad_Obj_Group_Type
+from Acad_Obj_Group import Acad_Obj_Group
