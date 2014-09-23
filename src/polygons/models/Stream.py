aec13ffb0e304ff122479460eead842ee184bf8d
Matt <mciancio@live.com.au>
2014-09-23 10:46:56 +1000

    Removed the description field of the Stream model, because it's not really necessary at this stage.

--- src/polygons/models/Stream.py
+++ src/polygons/models/Stream.py
@@ -4,7 +4,6 @@ class Stream(models.Model):
     
     name = models.TextField()
     code = models.CharField(max_length=6, unique=True)
-    description = models.TextField(null=True)
     
     class Meta:
         app_label = 'polygons'
