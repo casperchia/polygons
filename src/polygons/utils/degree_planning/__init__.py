4000046dc74f410324c14419a9e017b5a7e3ee50
Matt <mciancio@live.com.au>
2014-09-28 15:31:19 +1000

    Wrote a utility method to extract all core subjects from a program, but need to implement the method it depends on, which expands a subject rule input to produce a list of subjects.

--- src/polygons/utils/degree_planning/__init__.py
+++ src/polygons/utils/degree_planning/__init__.py
@@ -1,2 +1,28 @@
+from polygons.models.Program_Rule import Program_Rule
+from polygons.models.Stream_Group_Member import Stream_Group_Member
+from polygons.models.Stream_Rule import Stream_Rule
+from polygons.models.Rule import Rule
+from polygons.models.Subject import Subject
+
+def expand_subject_rule(rule):
+    pass
+
 def get_core_subjects(program):
-    raise Exception('Please implement me!')
+    subjects = Subject.objects.none()
+    
+    program_rules = Program_Rule.objects.filter(program=program).values_list('rule', flat=True)
+    core_subject_rules = Rule.objects.filter(id__in=program_rules, type__abbreviation='CC')
+    ds_rules = Rule.objects.filter(id__in=program_rules, type__abbreviation='DS')
+            
+    for ds_rule in ds_rules:
+        stream_members = Stream_Group_Member.objects.select_related('stream').filter(acad_obj_group=ds_rule.acad_obj_group)
+        
+        for stream in [sm.stream for sm in stream_members]:
+            stream_rules = Stream_Rule.objects.filter(stream=stream).values_list('rule', flat=True)
+        
+        core_subject_rules += Rule.objects.filter(id__in=stream_rules, type__abbreviation='CC')
+        
+    for rule in core_subject_rules:
+        subjects += expand_subject_rule(rule)
+    
+    return subjects
