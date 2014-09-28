eee714c4c720bc398c6e019c2406605addb9abe4
Matt <mciancio@live.com.au>
2014-09-28 21:14:00 +1000

    Fixed the tests, because they were calling the method which the view called and hence nothing was actually being tested. Testing for this needs to be manual like it is now.

--- src/polygons/tests/views/test_program_details.py
+++ src/polygons/tests/views/test_program_details.py
@@ -1,15 +1,17 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.db.models import Q
 
 from polygons.models.Program import Program
-from polygons.utils.degree_planning import get_core_subjects
+from polygons.models.Subject import Subject
 from polygons.messages import INVALID_DEGREE
         
 class Test_Program_Details(TestCase):
     urls = 'comp4920.urls'
     fixtures = ['Acad_Obj_Group_Type.json', 'Acad_Obj_Group.json',
                 'Org_Unit_Type.json', 'Org_Unit.json', 'Career.json',
-                'Degree.json', 'Program.json', 'Program_Group_Member.json']
+                'Degree.json', 'Program.json', 'Program_Group_Member.json',
+                'Subject.json']
     
     def test_status_code(self):
         
@@ -108,9 +110,16 @@ class Test_Program_Details(TestCase):
         
         print ('Testing that the page contains all the core subjects for the '
                'program.')
-        for subject in get_core_subjects(program):
+        subject_ids = [1862,1864,2212,1880,1882,1891,2179,2180,1892,1915,1921,
+                       1922,1923,1867,1868,1881,2178,2180,3017,3019]
+        for subject in Subject.objects.filter(id__in=subject_ids):
             self.assertContains(response, str(subject), status_code=200)
             self.assertContains(response, subject.id, status_code=200)
+            
+        print ('Testing that the page contains no other subjects.')
+        for subject in Subject.objects.filter(~Q(id__in=subject_ids)):
+            self.assertNotContains(response, str(subject), status_code=200)
+            self.assertNotContains(response, subject.id, status_code=200)
             
         print 'Visiting the program details page for Software Engineering.'
         program = Program.objects.get(name='Software Engineering')
@@ -123,11 +132,16 @@ class Test_Program_Details(TestCase):
         print 'Testing that the page contains the program text representation.'
         self.assertContains(response, str(program), status_code=200)
         
-        print ('Testing that the page contains all the core subjects for the '
-               'program.')
-        for subject in get_core_subjects(program):
+        subject_ids = [1862,1864,2212,3007,3931,1865,1866,1867,1868,3028,3036,
+                       10498,10499,1874,1884,1885,10500,3940,3941,3942]
+        for subject in Subject.objects.filter(id__in=subject_ids):
             self.assertContains(response, str(subject), status_code=200)
             self.assertContains(response, subject.id, status_code=200)
+            
+        print ('Testing that the page contains no other subjects.')
+        for subject in Subject.objects.filter(~Q(id__in=subject_ids)):
+            self.assertNotContains(response, str(subject), status_code=200)
+            self.assertNotContains(response, subject.id, status_code=200)
             
         print 'Visiting the program details page for Computer Science.'
         program = Program.objects.get(name='Computer Science')
@@ -140,11 +154,15 @@ class Test_Program_Details(TestCase):
         print 'Testing that the page contains the program text representation.'
         self.assertContains(response, str(program), status_code=200)
         
-        print ('Testing that the page contains all the core subjects for the '
-               'program.')
-        for subject in get_core_subjects(program):
+        subject_ids = [1862,1864,3007,1892,3942,1865,1867,1868]
+        for subject in Subject.objects.filter(id__in=subject_ids):
             self.assertContains(response, str(subject), status_code=200)
             self.assertContains(response, subject.id, status_code=200)
+            
+        print ('Testing that the page contains no other subjects.')
+        for subject in Subject.objects.filter(~Q(id__in=subject_ids)):
+            self.assertNotContains(response, str(subject), status_code=200)
+            self.assertNotContains(response, subject.id, status_code=200)
             
         print 'Visiting the program details page for Bioinformatics.'
         program = Program.objects.get(name='Bioinformatics')
@@ -157,11 +175,16 @@ class Test_Program_Details(TestCase):
         print 'Testing that the page contains the program text representation.'
         self.assertContains(response, str(program), status_code=200)
         
-        print ('Testing that the page contains all the core subjects for the '
-               'program.')
-        for subject in get_core_subjects(program):
+        subject_ids = [1437,1580,1862,1864,1589,1865,1868,2212,3007,1922,1923,
+                       1582,1583,1872,1884,1921,7808]
+        for subject in Subject.objects.filter(id__in=subject_ids):
             self.assertContains(response, str(subject), status_code=200)
             self.assertContains(response, subject.id, status_code=200)
+            
+        print ('Testing that the page contains no other subjects.')
+        for subject in Subject.objects.filter(~Q(id__in=subject_ids)):
+            self.assertNotContains(response, str(subject), status_code=200)
+            self.assertNotContains(response, subject.id, status_code=200)
             
         # ----- BEGIN INVALID DATA TESTING -----
         
