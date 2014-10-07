f75b2aa67983fe76edd827fc0a6bde1d0c262209
Corey <corey@corey-Satellite-A100.(none)>
2014-10-07 16:17:32 +1100

    degree title displayed on planner page

--- src/polygons/views/planner.py
+++ src/polygons/views/planner.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+
+from polygons.utils.views import get_cse_programs
+from polygons.models.Program import Program
+from polygons.messages import INVALID_DEGREE
+
 
 def planner(request):
-    return render_to_response('html/planner.html', 
-        context_instance=RequestContext(request))
+    prgm_id = request.POST.get("prgrm_pk")
+    try:
+        program = get_cse_programs().get(id=prgm_id)
+    except Program.DoesNotExist:
+        messages.error(request, INVALID_DEGREE)
+        return HttpResponseRedirect(reverse('polygons.views.degree_list'))
+
+    return render_to_response('html/planner.html',
+                              {
+                                'program' : program
+                              }, 
+                                context_instance=RequestContext(request))
