c77cda6d3e058c80db6d2d985a786367089289ce
Corey <corey@corey-Satellite-A100.(none)>
2014-10-08 23:39:24 +1100

    fixed urls and views

--- src/comp4920/urls.py
+++ src/comp4920/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('polygons.views',
     url(r'^$', 'index'),
     url(r'^degrees/$', 'degree_list'),
     url(r'^degree/(?P<program_id>\d+)/$', 'program_details'),
+    url(r'^review_page[/]?$', 'review_page'),
     url(r'^planner/$', 'planner'),
 )
