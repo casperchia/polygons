840c3ef553853603728d27630c155b75a1c3f658
Corey <corey@corey-Satellite-A100.(none)>
2014-10-10 01:20:33 +1100

    Created tests, added review description

--- src/polygons/tests/views/test_review_page.py
+++ src/polygons/tests/views/test_review_page.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import reverse
 from django.db.models import Q
 from polygons.models.Subject import Subject
 
-class Test_Program_Details(TestCase):
+class Test_Review_Page(TestCase):
     urls = 'comp4920.urls'
     fixtures = ['Acad_Obj_Group_Type.json', 'Acad_Obj_Group.json',
                 'Org_Unit_Type.json', 'Org_Unit.json', 'Career.json',
@@ -42,3 +42,15 @@ class Test_Program_Details(TestCase):
         subjects = Subject.objects.all()
         for subject in subjects:
             self.assertContains(response, str(subject.code), status_code=200)
+            
+        print 'Test that only subjects filter by one letter are displayed'
+        response = self.client.get(url + '/W')
+        for subject in Subject.objects.filter(code__startswith='W'):
+            self.assertContains(response, str(subject), status_code=200)
+            self.assertContains(response, subject.id, status_code=200)            
+            
+        print ('Testing that the page contains no other subjects.')
+        for subject in Subject.objects.filter(~Q(code__startswith='W')):
+            self.assertNotContains(response, str(subject), status_code=200)
+            self.assertNotContains(response, subject.id, status_code=200)
+            
