62fbad5aff9b7bcdad26b7e282f13330e457b41b
Corey <corey@corey-Satellite-A100.(none)>
2014-10-09 11:16:29 +1100

    Review page has alphabetic hyperlinks working

--- src/comp4920/urls.py
+++ src/comp4920/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('polygons.views',
     url(r'^degrees/$', 'degree_list'),
     url(r'^degree/(?P<program_id>\d+)/$', 'program_details'),
     url(r'^review_page[/]?$', 'review_page'),
+    url(r'^review_page/(?P<letter>[A-PR-WZ])$', 'review_page'),
     url(r'^planner/$', 'planner'),
 )
