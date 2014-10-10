9ea368fdc7bebef9bd5c71a1be73b2658cb644dd
luna_p <luna.pradhananga@gmail.com>
2014-10-11 01:48:43 +1100

    Started the view to display the year and semester

---
+++ src/polygons/views/display_info.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from polygons.models.Program_Plan import Program_Plan
+from polygons.models.Semester_Plan import Semester_Plan
+
+def display_info(request):
+       program_id = request.POST.get("prgrm_pk")
+       program_plan = Program_plan.objects.get(program=program_id)
+       semester_plan= Semester_plan.objects.get(program_plan=program_plan)
+       return render_to_response('html/display.html',
+                                {'program_plan' : program_plan,
+                                 'semester_plan' : semester_plan
+                                }
