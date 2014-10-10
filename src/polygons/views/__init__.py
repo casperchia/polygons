7a5a3e081de35a16414f5652a7e16a3e1257296c
Corey <corey@corey-Satellite-A100.(none)>
2014-10-10 23:23:02 +1100

    changed urls and view init

--- src/polygons/views/__init__.py
+++ src/polygons/views/__init__.py
@@ -2,4 +2,5 @@ from index import index
 from degree_list import degree_list
 from program_details import program_details
 from program_plan import program_plan
+from review_page import review_page
 
