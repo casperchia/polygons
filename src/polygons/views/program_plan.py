4af7eaf2f2454892ff3b16815d9a27396eaa3272
luna_p <luna.pradhananga@gmail.com>
2014-10-13 00:51:26 +1100

    Displaying the year and semester

--- src/polygons/views/program_plan.py
+++ src/polygons/views/program_plan.py
@@ -5,17 +5,23 @@ from django.core.urlresolvers import reverse
 from django.contrib import messages
 
 from polygons.models.Program_Plan import Program_Plan
+from polygons.models.Semester_Plan import Semester_Plan
 from polygons.messages import INVALID_PROGRAM_PLAN
 
 def program_plan(request, program_plan_id):
     try:
         program_plan = Program_Plan.objects.get(id=program_plan_id)
+        current_year = program_plan.current_year
+        no_of_semester = program_plan.current_semester.id
+     
     except Program_Plan.DoesNotExist:
         messages.error(request, INVALID_PROGRAM_PLAN)
         return HttpResponseRedirect(reverse('polygons.views.index'))
     
     return render_to_response('html/program_plan.html',
                              {
-                                'program_plan' : program_plan
+                                'program_plan' : program_plan, 
+                                'no_of_year' : range(current_year),
+                                'no_of_semester': range(no_of_semester)
                              },  
                              context_instance=RequestContext(request))
