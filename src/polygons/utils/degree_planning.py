05cea44f96f2b8802a5641a24e8ce34cd08601e2
Matt <mciancio@live.com.au>
2014-10-15 13:19:31 +1100

    Wrote the Python wrapper function that calls the currently unimplemented PLpgSQL function that determines which subjects in a program plan will be cascade deleted if a user removes a subject from the plan (because of prerequisites).

--- src/polygons/utils/degree_planning.py
+++ src/polygons/utils/degree_planning.py
@@ -1,6 +1,8 @@
 from django.db import connection
+from django.db.models import Q
 
 from polygons.models.Subject import Subject
+from polygons.models.Semester_Plan import Semester_Plan
 
 def get_core_subjects(program):
     with connection.cursor() as cursor:
@@ -19,6 +21,24 @@ def get_program_subjects(program, semester,
     with connection.cursor() as cursor:
         cursor.execute('select get_program_subjects(%s, %s, %s)',
                        [program.id, semester.id, list(existing_subjects)])
+        results = cursor.fetchall()
+    
+    if results:
+        subject_ids = [item[0] for item in results]
+    else:
+        subject_ids = []
+    
+    return Subject.objects.filter(id__in=subject_ids)
+
+def get_dependent_subjects(program_plan, pending_subject):
+    existing_subjects = Semester_Plan.objects.filter(~Q(subject=pending_subject),
+                                                     program_plan=program_plan).values_list('subject',
+                                                                                            flat=True)
+    
+    with connection.cursor() as cursor:
+        cursor.execute('select get_dependent_subjects(%s, %s, %s)',
+                       [program_plan.program_id, pending_subject.id,
+                        existing_subjects])
         results = cursor.fetchall()
     
     if results:
