b0d539292a473f1f7581e6d6368e08e90f66a613
Matt <mciancio@live.com.au>
2014-10-18 10:48:15 +1100

    Wrote a utility function that should hopefully render django HTML templates into PDF responses.

--- src/polygons/utils/views.py
+++ src/polygons/utils/views.py
@@ -1,3 +1,10 @@
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+
+from xhtml2pdf import pisa
+import StringIO
+
 from polygons.models.Program import Program
 from polygons.models.Program_Group_Member import Program_Group_Member
 
@@ -6,4 +13,22 @@ _CSE_PLANS_ID = 20382
 def get_cse_programs():
     ids = Program_Group_Member.objects.filter(acad_obj_group=_CSE_PLANS_ID).values_list('program',
                                                                                         flat=True)
-    return Program.objects.filter(id__in=ids) 
+    return Program.objects.filter(id__in=ids)
+
+def render_to_pdf(template_path, context_data, file_name):
+    template = get_template(template_path)
+    context = Context(context_data)
+    html = template.render(context)
+    
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="%s.pdf"'%file_name
+    
+    pdf_output = StringIO.StringIO()
+    
+    success = pisa.CreatePDF(html, dest=pdf_output)
+    if not success:
+        raise Exception('Failed to render "%s" PDF!'%file_name)
+    
+    response.write(pdf_output)
+    
+    return response
