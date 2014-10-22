849a92a17b78bcd53bea87b0c087d8708fe0595e
luna_p <luna.pradhananga@gmail.com>
2014-10-22 14:31:29 +1100

    Delete program_plan included

--- src/polygons/forms/add_semester.py
+++ src/polygons/forms/add_semester.py
@@ -8,14 +8,13 @@ from polygons.models.Semester import Semester
 class New_Semester_Form(forms.Form):
 
     def save(self, program_plan):
+        plan = Program_Plan.objects.get(id = program_plan.id)
         semester1 = Semester.objects.get(abbreviation='S1')
         semester2 = Semester.objects.get(abbreviation='S2')
-        year = program_plan.current_year
         if (program_plan.current_semester == semester2):
             program_plan.current_semester = semester1
-            year += 1
+            program_plan.current_year += 1
         else:
             program_plan.current_semester = semester2
-        program_plan.current_year = year
         program_plan.save()
         return program_plan
