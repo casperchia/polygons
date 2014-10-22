0ca1d8a631e1b6b382e40a5cab380588c44f8ee2
andrew <andrew@ubuntu.ubuntu-domain>
2014-10-22 20:54:46 +1100

    removing a course will now also remove dependent courses

--- src/polygons/forms/remove_from_plan.py
+++ src/polygons/forms/remove_from_plan.py
@@ -2,6 +2,7 @@ import django.forms as forms
 
 from polygons.models.Semester_Plan import Semester_Plan
 from polygons.models.Subject import Subject
+from polygons.utils.degree_planning import get_dependent_subjects
 
 class Remove_From_Plan_Form(forms.Form):
     
@@ -12,6 +13,12 @@ class Remove_From_Plan_Form(forms.Form):
         year = program_plan.current_year
         semester = program_plan.current_semester
         subject = Subject.objects.get(id=request.POST.get("subject"))
+        
+        dependent_subjects = get_dependent_subjects(program_plan, subject)
+        
+        semester_plan = Semester_Plan.objects.filter(program_plan=program_plan, year=year, 
+                                                     semester=semester, subject=dependent_subjects)
+        semester_plan.delete()
         semester_plan = Semester_Plan.objects.filter(program_plan=program_plan, year=year, 
                                                      semester=semester, subject=subject)
         semester_plan.delete()
