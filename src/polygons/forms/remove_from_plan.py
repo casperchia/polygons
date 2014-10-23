c6b085b6ff8ba1d02b00dc3e0dfd2af03c2c1474
andrew <andrew@ubuntu.ubuntu-domain>
2014-10-23 12:55:28 +1100

    removing courses now deducts uoc from total uoc tally

--- src/polygons/forms/remove_from_plan.py
+++ src/polygons/forms/remove_from_plan.py
@@ -13,9 +13,12 @@ class Remove_From_Plan_Form(forms.Form):
         year = program_plan.current_year
         semester = program_plan.current_semester
         subject = Subject.objects.get(id=request.POST.get("subject"))
-        
+        program_plan.uoc_tally -= subject.uoc
         dependent_subjects = get_dependent_subjects(program_plan, subject)
-        
+        for s in dependent_subjects:
+            program_plan.uoc_tally -= s.uoc
+            
+        print program_plan.uoc_tally
         semester_plan = Semester_Plan.objects.filter(program_plan=program_plan, year=year, 
                                                      semester=semester, subject=dependent_subjects)
         semester_plan.delete()
