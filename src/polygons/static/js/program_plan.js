81150ec5ace1819949642c8c43adaeacb37f4538
Matt <mciancio@live.com.au>
2014-10-24 16:40:55 +1100

    Implemented the JavaScript function to vertically center the popup window on the page.

--- src/polygons/static/js/program_plan.js
+++ src/polygons/static/js/program_plan.js
@@ -1,5 +1,22 @@
+function document_height() {
+   var result = Math.max(
+      document.documentElement.clientHeight,
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.offsetHeight
+   );
+   return px_to_num(result);
+}
+
 function center_popup_window() {
-   // TODO
+   var popup = document.getElementById('remove_course_popup');
+   var popup_height = popup.currentStyle ? popup.currentStyle.height :
+                                           getComputedValue(popup, null).height;
+   popup_height = px_to_num(popup_height);
+   var total_height = document_height();
+
+   popup.style.marginTop = (total_height - popup_height) / 2 + 'px';
 }
 
 function populate_dependent_subjects(program_plan_id, subject_id) {
@@ -12,8 +29,8 @@ function populate_dependent_subjects(program_plan_id, subject_id) {
 }
 
 function display_remove_course_popup(program_plan_id, subject_id) {
-   center_popup_window();
    document.body.style.overflowY = 'hidden'; // Disable scrolling
+   center_popup_window();
    populate_dependent_subjects(program_plan_id, subject_id);
    document.getElementById('remove_course_field').value = subject_id;
    document.getElementById('remove_course_popup_container').style.display = 'block';
