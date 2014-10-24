c69276d287c239a666111076cb68627dbc078e2f
Matt <mciancio@live.com.au>
2014-10-25 10:20:46 +1100

    Missing comma in tuple.

--- src/comp4920/urls.py
+++ src/comp4920/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('polygons.views',
     url(r'^degree/plan/course/add/$', 'add_course'),
     url(r'^degree/plan/course/back/$', 'back_to_plan'),
     url(r'^degree/plan/semester/(?P<program_plan_id>\d+)/$', 'new_semester'),
-    url(r'^degree/plan/remove-course/(?P<program_plan_id>\d+)/$', 'remove_course')
+    url(r'^degree/plan/remove-course/(?P<program_plan_id>\d+)/$', 'remove_course'),
     url(r'^degree/plan/dependent-subjects/(?P<program_plan_id>\d+)/(?P<subject_id>\d+)/',
         'fetch_dependent_subjects')
 )
