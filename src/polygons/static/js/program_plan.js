87928e468e3f149d6f08ad345109372ace3b3bf9
Matt <mciancio@live.com.au>
2014-10-24 16:32:35 +1100

    Finished the highest level JavaScript function for the popup window.

--- src/polygons/static/js/program_plan.js
+++ src/polygons/static/js/program_plan.js
@@ -1,9 +1,20 @@
-function display_remove_course_popup(program_plan_id, subject_id) {
+function center_popup_window() {
+   // TODO
+}
+
+function populate_dependent_subjects(program_plan_id, subject_id) {
+   // TODO
    /*
-      * Center the pop up window
-      * Set the overflow-y of the body to hidden
-      * Make the AJAX call to populate the list of dependent subjects
-      * Set the subject input value using the received subject_id
-      * Display the popup window container
+      * Make AJAX call to get list of dependent subjects
+      * If list is not empty, unhide the dependent_subjects div
+      * if the list is not empty, insert <li> into the dependent_subjects list
    */
+}
+
+function display_remove_course_popup(program_plan_id, subject_id) {
+   center_popup_window();
+   document.body.style.overflowY = 'hidden'; // Disable scrolling
+   populate_dependent_subjects(program_plan_id, subject_id);
+   document.getElementById('remove_course_field').value = subject_id;
+   document.getElementById('remove_course_popup_container').style.display = 'block';
 }
