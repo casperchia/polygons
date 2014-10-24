8d79643b1eabe8fa25b0224912ce0d5df77eddbc
Matt <mciancio@live.com.au>
2014-10-24 16:20:47 +1100

    Created a container for the popup window to make the background have a transluscent overlay.

---
+++ src/polygons/static/js/program_plan.js
@@ -0,0 +1,9 @@
+function display_remove_course_popup(program_plan_id, subject_id) {
+   /*
+      * Center the pop up window
+      * Set the overflow-y of the body to hidden
+      * Make the AJAX call to populate the list of dependent subjects
+      * Set the subject input value using the received subject_id
+      * Display the popup window container
+   */
+}
