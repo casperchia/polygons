76d2840202fa5470ebc7fe890c58123950b5b7f2
Matt <mciancio@live.com.au>
2014-10-25 13:46:08 +1100

    Changed the implementation of the filter form transition so that it shouldn't appear for a split second when the page loads.

--- src/polygons/static/js/add_course.js
+++ src/polygons/static/js/add_course.js
@@ -5,17 +5,20 @@ function toggle_filter_display() {
    var height = filter_form.currentStyle ? filter_form.currentStyle.height :
                                            getComputedStyle(filter_form, null).height;
    if (height == '0px') {
-      filter_form.style.height = filter_form_height;
+      filter_form.style.height = filter_form_height + 'px';
    } else {
+      filter_form.style.overflow = 'hidden';
       filter_form.style.height = '0';
    }
 }
 
 function capture_filter_height() {
    var filter_form = document.getElementById('filter_form');
-   filter_form_height = filter_form.currentStyle ? filter_form.currentStyle.height :
-                                                   getComputedStyle(filter_form, null).height;
+   filter_form_height = filter_form.clientHeight;
    filter_form.style.height = '0';
+   filter_form.style.position = 'static';
+   filter_form.style.visibility = 'visible';
+   filter_form.style.overflow = 'hidden';
    filter_form.className += ' filter_form_transition';
 }
 
